62d4b173c5ac1149b6c8a662bdf5b940ad7bee65
Tarik Ansari <tarikjn@gmail.com>
2011-02-07 22:53:15 -0800

    updated stating environment settings

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -6,9 +6,9 @@ Mojo::Application.configure do
   config.cache_classes = true
 
   # Full error reports are on and caching is turned on
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true
   config.action_view.debug_rjs             = true
-  config.action_controller.perform_caching = true
+  config.action_controller.perform_caching = false
 
   # Specifies the header that your server uses for sending files
   config.action_dispatch.x_sendfile_header = "X-Sendfile"
