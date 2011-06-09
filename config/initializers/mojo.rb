e616ddf4fa4a55f165c68b558b1954d044e561ef
Tarik Ansari <tarik@mojo.co>
2011-06-09 15:06:39 -0700

    S3/fog host fix

--- config/initializers/mojo.rb
+++ config/initializers/mojo.rb
@@ -15,7 +15,7 @@ CarrierWave.configure do |config|
   }
   config.fog_directory  = SETTINGS[Rails.env]['bucket']                     # required
   # TODO: fix to simply use http? or use amazon domain
-  config.fog_host       = 'http://dev-media.mojo.co.s3.amazonaws.com'            # optional, defaults to nil
+  #config.fog_host       = 'http://dev-media.mojo.co.s3.amazonaws.com'            # optional, defaults to nil
   #config.fog_public     = false                                   # optional, defaults to true
   #config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
