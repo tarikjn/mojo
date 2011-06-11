271a900b2688d93c3da859ae7919b3ccd998ec29
Tarik Ansari <tarik@mojo.co>
2011-06-10 17:40:42 -0700

    added staging host for mailer

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -47,4 +47,6 @@ Mojo::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  config.action_mailer.default_url_options = { :host => "staging.mojo.co" }
 end
