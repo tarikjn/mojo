95e7859331fd71cd6ad7b2d8f1ed06f9f027bbc3
Tarik Ansari <tarik@mojo.co>
2011-09-04 22:40:43 -0700

    added config.assets.digest = true, required for heroku

--- config/application.rb
+++ config/application.rb
@@ -43,7 +43,7 @@ module Mojo
     config.assets.enabled = true
     
     # Version of your assets, change this if you want to expire all your assets
-    #config.assets.version = '1.0'
+    config.assets.version = '1.0'
   end
 end
 
